c0ed6c954f0a0ca57c51888bb57af79f75f66045
THEPADA <padazue@googlemail.com>
2022-01-26 12:01:59 +0000

    Added foundation for crop scanning as action

---
+++ scripts/scan_crops_client.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# ----------------------------------
+# @author: gpdas
+# @email: pdasgautham@gmail.com
+# @date:
+# ----------------------------------
+
+import rospy
+import actionlib
+
+from uol_cmp9767m_tutorial.msg import DoDishesAction, DoDishesGoal
+
+class ScanCropsServer:
+    def __init__(self):
+        rospy.init_node('do_dishes_client')
+        self.client = actionlib.SimpleActionClient('scan_crop_line', DoDishesAction)
+        self.client.wait_for_server()
+    
+    def send_goal(self, target):
+        goal = DoDishesGoal()
+        #Fill in the goal here
+        self.client.send_goal(goal)
+        goal.target = target
+        status = self.client.wait_for_result(rospy.Duration.from_sec(5.0))
+        result = self.client.get_result()
+        rospy.loginfo("status is %s", status)
+        rospy.loginfo("result is %s", result)
---
+++ scripts/scan_crops_server.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# ----------------------------------
+# @author: gpdas
+# @email: pdasgautham@gmail.com
+# @date:
+# ----------------------------------
+
+import rospy
+import actionlib
+
+from uol_cmp9767m_tutorial.msg import ScanCropLineAction, ScanCropLineResult
+
+class ScanCropLinesServer:
+    def __init__(self):
+        self.server = actionlib.SimpleActionServer('scan_crops', ScanCropLineAction, self.execute, False)
+        self.server.start()
+        self.scaned_crop_lines = 0
+
+    def execute(self, goal):
+        # Do lots of robot stuff here
+        # total dishes count is incremented each time the action is executed, and returned as result
+        self.total_dishes_cleaned += 1
+        result = ScanCropLineResult()
+        result.total_dishes_cleaned = self.total_dishes_cleaned
+        self.server.set_succeeded(result)
+
+
+if __name__ == '__main__':
+    rospy.init_node('scan_cropline_server')
+    server = ScanCropLinesServer()
+    rospy.spin()
